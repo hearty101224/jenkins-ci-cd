bcab393e65a0535d49c114877aa27deb11fdebec
@harhar25 <harold.jey.madjos@aclcbutuan.edu.ph>
2025-03-22 22:55:14 +0800

    fixed test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,9 @@
 import unittest
-from app import app
+from app import add
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(app.add(2, 3), "The sum is 5")
+        self.assertEqual(add(2, 3), "The sum is 5")
 
 if __name__ == "__main__":
     unittest.main()
